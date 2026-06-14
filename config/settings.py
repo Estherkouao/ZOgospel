a3827edd2063b57656cea5cef3e0d7ff3b0989d4
Esther Kouao <estherkouao9@gmail.com>
2026-06-14 13:52:19 +0000

    change

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tzo=wg0)g@-)hv@t1bzuad!rj=@e$$kupmhgn+v=j7b@7rbcpa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
